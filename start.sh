1f364c14d969aca7076d661f6f7dbfc40384518b
Adithyan1233 <88931285+Adithyan1233@users.noreply.github.com>
2023-06-26 20:27:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Adithyan1233/DQ-the-file-donor.git /DQTheFileDonorBot
+  git clone https://github.com/lisaowner/DQ-the-file-donor.git /DQTheFileDonorBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /DQTheFileDonorBot
